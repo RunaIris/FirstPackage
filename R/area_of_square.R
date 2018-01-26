287671cc46f63a708afb1c318bc7cdcd554da605
fun-cursus11 <fun-cursus11@pc150218.knmi.nl>
2018-01-26 10:49:38 +0000

    added documentation

---
+++ R/area_of_square.R
@@ -0,0 +1,12 @@
+#'
+#'!@title Square Area
+#'@description function calculates area of square from width and length
+#'@param_width the width of the square
+#'@param_length the length of the square
+#'@author Runa Magnusson
+#'@export
+
+area_of_square <- function(length, width) {
+  area <- length*width
+  return(area)
+}
